f35676f3fb65662945018e70c3c082b9a6468182
MigueltrejoEsp <mangeel008@gmail.com`>
2026-08-16 15:14:41 -0300

    handle event

--- lib/heads_up_web/live/effort_live.ex
+++ lib/heads_up_web/live/effort_live.ex
@@ -11,6 +11,9 @@ defmodule HeadsUpWeb.EffortLive do
     <div class="effort">
       <h1>Community Love</h1>
       <section>
+        <button phx-click="add" phx-value-quantity="3">
+          + 3
+        </button>
         <div>
           {@responders}
         </div>
@@ -25,5 +28,10 @@ defmodule HeadsUpWeb.EffortLive do
       </section>
     </div>
     """
+  end
+
+  def handle_event("add", %{"quantity" => quantity}, socket) do
+    socket = update(socket, :responders, &(&1 + String.to_integer(quantity)))
+    {:noreply, socket}
   end
 end
