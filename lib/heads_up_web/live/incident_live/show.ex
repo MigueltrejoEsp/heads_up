1c715c8f472c2107f4848223e57e4c7e84d5749c
MigueltrejoEsp <mangeel008@gmail.com`>
2026-08-20 09:25:04 -0300

    live navigation reusing web socket connection

--- lib/heads_up_web/live/incident_live/show.ex
+++ lib/heads_up_web/live/incident_live/show.ex
@@ -57,7 +57,9 @@ defmodule HeadsUpWeb.IncidentLive.Show do
       <h4>Urgent Incidents</h4>
       <ul class="incidents">
         <li :for={incident <- @incidents}>
-          <img src={incident.image_path} /> {incident.name}
+          <.link navigate={~p"/incidents/#{incident}"}>
+            <img src={incident.image_path} /> {incident.name}
+          </.link>
         </li>
       </ul>
     </section>
